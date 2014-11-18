865e2e49e5d412cce370405090c24e185a8e4624
WangJun <wangjuntytl@163.com>
2014-11-18 17:11:01 +0800

    codeUpdate

--- run.sh
+++ run.sh
@@ -15,8 +15,11 @@ echo "构建环境：${ENV}"
 echo '----------------------------------------------'
 
 
+[ ! -n `which mvn` ] && "请先安装maven，并添加mvn到path变量中" && exit 1
+[ ! -n `which git` ] && "请先安装git，并添加git到path变量中"  && exit 1
+
 #下载依赖包，最好手动将依赖包install到你的本地仓库
-[ ! -d "peaceful-common-utils" ] && git clone git@github.com:WangJunTYTL/peaceful-parent.git
+[ ! -d "peaceful-common-utils" ] && echo "下载依赖包" && git clone git@github.com:WangJunTYTL/peaceful-parent.git
 [ ! -d "peaceful-common-utils" ] && git clone git@github.com:WangJunTYTL/peaceful-common-utils.git
 
 
