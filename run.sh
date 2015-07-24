ba209dd7f35bf90efc69d1b920d8eca1b5977d2b
wangjun <wangjun@edaijia-inc.cn>
2015-07-24 22:40:05 +0800

    valid user computer  git & mvn  cmd is install

--- run.sh
+++ run.sh
@@ -14,9 +14,10 @@ echo '----------------------------------------------'
 echo "构建环境：${ENV}"
 echo '----------------------------------------------'
 
-
-[ ! -n `which mvn` ] && "请先安装maven，并添加mvn到path变量中" && exit 1
-[ ! -n `which git` ] && "请先安装git，并添加git到path变量中"  && exit 1
+echo 'check mvn cmd... 'which mvn
+[ $? != 0 ] && "请先安装maven，并添加mvn到path变量中" && exit 1
+echo 'check git cmd... 'which git
+[ $? != 0 ] && "请先安装git，并添加git到path变量中"  && exit 1
 
 #下载依赖包，最好手动将依赖包install到你的本地仓库
 [ ! -d "peaceful-basic-platform" ]   && git clone https://github.com/WangJunTYTL/peaceful-basic-platform.git && echo "下载依赖包"
